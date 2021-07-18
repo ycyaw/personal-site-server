63a12966c07d4921447f6551bb03626503651bc8
Miracle@pro <luomengfanchen@outlook.com>
2021-07-18 12:05:07 +0800

    feat: 将日志输出到文件

--- log/log.go
+++ log/log.go
@@ -9,8 +9,14 @@ import (
 var logger *log.Logger
 
 func init() {
+	// 打开(新建)日志文件
+	logfile, err := os.OpenFile("personal-site.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
 	// 初始化日志文件格式
-	logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	logger = log.New(logfile, "", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 // [INFO]级别日志
